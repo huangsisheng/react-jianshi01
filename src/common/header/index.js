6a2b88974f4638fc23df1456745127736ca8e83c
huangsisheng <1173091704@qq.com>
2021-03-07 00:55:48 +0800

    格式化

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,7 +1,7 @@
 /*
-* @Author: your name
-* @Date: 2021-03-03 17:24:03
- * @LastEditTime: 2021-03-07 00:40:33
+ * @Author: your name
+ * @Date: 2021-03-03 17:24:03
+ * @LastEditTime: 2021-03-07 00:51:45
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \jianshu01\src\common\header\index.js
@@ -9,155 +9,162 @@
 import React, { Component } from "react";
 // 动画库
 import { CSSTransition } from "react-transition-group";
-import { connect } from "react-redux"
-import { actionCreators } from "./store"
+import { connect } from "react-redux";
+import { actionCreators } from "./store";
 
 import {
-    HeaderWrapper,
-    Logo,
-    Nav,
-    NavItem,
-    NavSearch,
-    Addtion,
-    Button,
-    SearchWrapper,
-    SearchInfo,
-    SearchInfoTitle,
-    SearchInfoSwitch,
-    SearchInfoItem,
-    SearchInfoList
+  HeaderWrapper,
+  Logo,
+  Nav,
+  NavItem,
+  NavSearch,
+  Addtion,
+  Button,
+  SearchWrapper,
+  SearchInfo,
+  SearchInfoTitle,
+  SearchInfoSwitch,
+  SearchInfoItem,
+  SearchInfoList,
 } from "./style";
 
-
 class Header extends Component {
-    // constructor(props) {
-    //     super(props)
-    // }
+  // constructor(props) {
+  //     super(props)
+  // }
 
-    getListArea() {
-        const {
-            focused,
-            mouseIn,
-            list,
-            page,
-            totalPage,
-            handleMouseEnter,
-            handleMouseLeave,
-            handleChangePage
-        } = this.props
-        let newList = list.toJS(), // 把immutable 对象转换为JS对象
-            eleList = []
-        if (newList.length) {
-            for (let i = (page - 1) * 10; i < page * 10; i++) {
-                eleList.push(<SearchInfoItem key={newList[i]}>{newList[i]}</SearchInfoItem>)
-            }
-        }
-        if (focused || mouseIn) {
-            return (
-                <SearchInfo
-                    onMouseEnter={handleMouseEnter}
-                    onMouseLeave={handleMouseLeave}>
-                    <SearchInfoTitle>热门搜索
-                        <SearchInfoSwitch
-                            onClick={() => { handleChangePage(page, totalPage, this.spinIcon) }}>
-                            <i ref={(icon) => { this.spinIcon = icon }} className={'iconfont spin'}>&#xe63a;</i>换一批
-                        </SearchInfoSwitch>
-                    </SearchInfoTitle>
-                    <SearchInfoList>
-                        {eleList}
-                    </SearchInfoList>
-                </SearchInfo>
-            )
-        } else {
-            return null
-        }
-    }
-
-    render() {
-        const {
-            focused,
-            handleFocus,
-            handleBlur,
-            list
-        } = this.props
-
-        return (
-            <HeaderWrapper>
-                <Logo href="/" />
-                <Nav>
-                    <NavItem className="left">首页</NavItem>
-                    <NavItem className="left">下载App</NavItem>
-                    <SearchWrapper>
-                        <CSSTransition
-                            in={focused}
-                            timeout={500}
-                            classNames="slide"
-                        >
-                            <NavSearch
-                                onFocus={() => { handleFocus(list) }}
-                                onBlur={handleBlur}
-                                className={focused ? 'focused' : ''} />
-                        </CSSTransition>
-                        <i className={focused ? 'focused iconfont zoom' : 'iconfont zoom'}>&#xe61c;</i>
-                        {this.getListArea(focused)}
-                    </SearchWrapper>
-                    <NavItem className="right">
-                        <i className="iconfont">&#xe636;</i>
-                    </NavItem>
-                    <NavItem className="right">登录</NavItem>
-                </Nav>
-                <Addtion>
-                    <Button className="sign-up">注册</Button>
-                    <Button className="write"><i className="iconfont">&#xe676;</i>写文章</Button>
-                </Addtion>
-            </HeaderWrapper>
+  getListArea() {
+    const {
+      focused,
+      mouseIn,
+      list,
+      page,
+      totalPage,
+      handleMouseEnter,
+      handleMouseLeave,
+      handleChangePage,
+    } = this.props;
+    let newList = list.toJS(), // 把immutable 对象转换为JS对象
+      eleList = [];
+    if (newList.length) {
+      for (let i = (page - 1) * 10; i < page * 10; i++) {
+        eleList.push(
+          <SearchInfoItem key={newList[i]}>{newList[i]}</SearchInfoItem>
         );
+      }
     }
+    if (focused || mouseIn) {
+      return (
+        <SearchInfo
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <SearchInfoTitle>
+            热门搜索
+            <SearchInfoSwitch
+              onClick={() => {
+                handleChangePage(page, totalPage, this.spinIcon);
+              }}
+            >
+              <i
+                ref={(icon) => {
+                  this.spinIcon = icon;
+                }}
+                className={"iconfont spin"}
+              >
+                &#xe63a;
+              </i>
+              换一批
+            </SearchInfoSwitch>
+          </SearchInfoTitle>
+          <SearchInfoList>{eleList}</SearchInfoList>
+        </SearchInfo>
+      );
+    } else {
+      return null;
+    }
+  }
 
+  render() {
+    const { focused, handleFocus, handleBlur, list } = this.props;
 
+    return (
+      <HeaderWrapper>
+        <Logo href="/" />
+        <Nav>
+          <NavItem className="left">首页</NavItem>
+          <NavItem className="left">下载App</NavItem>
+          <SearchWrapper>
+            <CSSTransition in={focused} timeout={500} classNames="slide">
+              <NavSearch
+                onFocus={() => {
+                  handleFocus(list);
+                }}
+                onBlur={handleBlur}
+                className={focused ? "focused" : ""}
+              />
+            </CSSTransition>
+            <i className={focused ? "focused iconfont zoom" : "iconfont zoom"}>
+              &#xe61c;
+            </i>
+            {this.getListArea(focused)}
+          </SearchWrapper>
+          <NavItem className="right">
+            <i className="iconfont">&#xe636;</i>
+          </NavItem>
+          <NavItem className="right">登录</NavItem>
+        </Nav>
+        <Addtion>
+          <Button className="sign-up">注册</Button>
+          <Button className="write">
+            <i className="iconfont">&#xe676;</i>写文章
+          </Button>
+        </Addtion>
+      </HeaderWrapper>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
-    return {
-        // 使用getIn方法获取immutable中的值,那个模块下对应的那个值
-        focused: state.getIn(['header', 'focused']),
-        list: state.getIn(['header', 'list']),
-        mouseIn: state.getIn(['header', 'mouseIn']),
-        page: state.getIn(['header', 'page']),
-        totalPage: state.getIn(['header', 'totalPage']),
-    }
-}
+  return {
+    // 使用getIn方法获取immutable中的值,那个模块下对应的那个值
+    focused: state.getIn(["header", "focused"]),
+    list: state.getIn(["header", "list"]),
+    mouseIn: state.getIn(["header", "mouseIn"]),
+    page: state.getIn(["header", "page"]),
+    totalPage: state.getIn(["header", "totalPage"]),
+  };
+};
 const mapDispatchToProps = (dispatch) => {
-    return {
-        handleFocus(list) {
-            console.log(list)
-            // 默认会把dispatch 作为参数传递进去，redux-thunk
-            list.size === 0 && dispatch(actionCreators.getList())
-            dispatch(actionCreators.searchFocus())
-        },
-        handleBlur() {
-            dispatch(actionCreators.searchBlur())
-        },
-        handleMouseEnter() {
-            dispatch(actionCreators.mouseEnter())
-        },
-        handleMouseLeave() {
-            dispatch(actionCreators.mouseLeave())
-        },
-        handleChangePage(page, totalPage, el) {
-            let originAngle = el.style.transform.replace(/[^0-9]/gi, '')
-            if (originAngle) {
-                originAngle = parseInt(originAngle)
-            } else {
-                originAngle = 0
-            }
-            el.style.transform = `rotate(${originAngle + 360}deg)`
-            page < totalPage
-                ? dispatch(actionCreators.changePage(page + 1))
-                : dispatch(actionCreators.changePage(1))
-        }
-    }
-}
+  return {
+    handleFocus(list) {
+      console.log(list);
+      // 默认会把dispatch 作为参数传递进去，redux-thunk
+      list.size === 0 && dispatch(actionCreators.getList());
+      dispatch(actionCreators.searchFocus());
+    },
+    handleBlur() {
+      dispatch(actionCreators.searchBlur());
+    },
+    handleMouseEnter() {
+      dispatch(actionCreators.mouseEnter());
+    },
+    handleMouseLeave() {
+      dispatch(actionCreators.mouseLeave());
+    },
+    handleChangePage(page, totalPage, el) {
+      let originAngle = el.style.transform.replace(/[^0-9]/gi, "");
+      if (originAngle) {
+        originAngle = parseInt(originAngle);
+      } else {
+        originAngle = 0;
+      }
+      el.style.transform = `rotate(${originAngle + 360}deg)`;
+      page < totalPage
+        ? dispatch(actionCreators.changePage(page + 1))
+        : dispatch(actionCreators.changePage(1));
+    },
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Header);
-
--- src/common/header/style.js
+++ src/common/header/style.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-03-03 17:29:46
- * @LastEditTime: 2021-03-07 00:07:16
+ * @LastEditTime: 2021-03-07 00:51:17
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \jianshu01\src\common\header\style.js
@@ -29,159 +29,159 @@ export const Nav = styled.nav`
   border-color: #e7e7e7;
   width: 960px;
   height: 56px;
-@media (min-width: 768px){
+  @media (min-width: 768px) {
     width: 750px;
-}
-@media (min-width: 992px){
+  }
+  @media (min-width: 992px) {
     width: 960px;
-}
-@media (max-width: 1080px){
+  }
+  @media (max-width: 1080px) {
     width: 750px;
-}
+  }
 `;
 
 export const NavItem = styled.div`
-    line-height: 56px;
-    padding:0 15px;
-    font-size:17px;
-    color:#333;
-    cursor: pointer;
+  line-height: 56px;
+  padding: 0 15px;
+  font-size: 17px;
+  color: #333;
+  cursor: pointer;
 
-    &.left{
-        float: left;
-        color:#969696;
-    }
-    &.right{
-        float: right;
-        color:#969696;
-    }
-`
+  &.left {
+    float: left;
+    color: #969696;
+  }
+  &.right {
+    float: right;
+    color: #969696;
+  }
+`;
 
 export const SearchWrapper = styled.div`
-    position:relative;
-    float:left;
-    .zoom{
-        position:absolute;
-        right:5px;
-        bottom:11px;
-        width:30px;
-        height:30px;
-        line-height:30px;
-        border-radius:50%;
-        text-align:center;
-        cursor: pointer;
-        &.focused{
-            background-color:#969696;
-            color:#fff;
-        }
+  position: relative;
+  float: left;
+  .zoom {
+    position: absolute;
+    right: 5px;
+    bottom: 11px;
+    width: 30px;
+    height: 30px;
+    line-height: 30px;
+    border-radius: 50%;
+    text-align: center;
+    cursor: pointer;
+    &.focused {
+      background-color: #969696;
+      color: #fff;
     }
-`
+  }
+`;
 export const NavSearch = styled.input.attrs({
-    placeholder: '搜索'
+  placeholder: "搜索",
 })`
-    padding: 0 42px 0 20px;
+  padding: 0 42px 0 20px;
+  width: 160px;
+  height: 38px;
+  font-size: 14px;
+  border: 1px solid #eee;
+  border-radius: 40px;
+  background: #eee;
+  transition: width 0.5s;
+  outline: none;
+  color: #777;
+  &::placeholder {
+    color: #999;
+  }
+  &.focused {
+    width: 240px;
+  }
+  &.slide-enter {
+    transition: all 0.5s ease-out;
     width: 160px;
-    height: 38px;
-    font-size: 14px;
-    border: 1px solid #eee;
-    border-radius: 40px;
-    background: #eee;
-    transition: width .5s;
-    outline: none;
-    color:#777;
-    &::placeholder{
-        color:#999;
-    }
-    &.focused{
-        width: 240px;
-    }
-    &.slide-enter{
-        transition:all 0.5s ease-out;
-        width: 160px;
-    }
-    &.slide-enter-active{
-        width: 240px;
-    }
-    &.slide-exit{
-        transition:all 0.5s ease-out;
-        width: 240px;
-    }
-    &.slide-exit-active{
-        width: 160px;
-    }
-`
+  }
+  &.slide-enter-active {
+    width: 240px;
+  }
+  &.slide-exit {
+    transition: all 0.5s ease-out;
+    width: 240px;
+  }
+  &.slide-exit-active {
+    width: 160px;
+  }
+`;
 
 export const SearchInfo = styled.div`
-    position:absolute;
-    left:0;
-    top:56px;
-    width:240px;
-    padding:0 20px;
-    background-color: #fff;
-    box-shadow: 0 0 8px rgb(0 0 0 / 20%);
-`
+  position: absolute;
+  left: 0;
+  top: 56px;
+  width: 240px;
+  padding: 0 20px;
+  background-color: #fff;
+  box-shadow: 0 0 8px rgb(0 0 0 / 20%);
+`;
 export const SearchInfoTitle = styled.div`
-    margin-top:20px;
-    margin-bottom:15px;
-    line-height:20px;
-    font-size:14px;
-    color:#969696;
-`
+  margin-top: 20px;
+  margin-bottom: 15px;
+  line-height: 20px;
+  font-size: 14px;
+  color: #969696;
+`;
 export const SearchInfoSwitch = styled.span`
-    float:right;
-    font-size:13px;
-    cursor: pointer;
-    .spin{
-        display:block;
-        float:left;
-        margin-right:2px;
-        font-size:12px;
-        transition: all .2s ease-in;
-        transform-origin:center center;
-    }
-`
+  float: right;
+  font-size: 13px;
+  cursor: pointer;
+  .spin {
+    display: block;
+    float: left;
+    margin-right: 2px;
+    font-size: 12px;
+    transition: all 0.2s ease-in;
+    transform-origin: center center;
+  }
+`;
 export const SearchInfoList = styled.span`
-    overflow:hidden;
-`
+  overflow: hidden;
+`;
 export const SearchInfoItem = styled.a`
-    display:block;
-    float:left;
-    line-height:20px;
-    padding:0 5px;
-    color:#787878;
-    border:1px solid #ddd;
-    font-size:12px;
-    border-radius:3px;
-    margin-right:10px;
-    margin-bottom:10px;
-`
+  display: block;
+  float: left;
+  line-height: 20px;
+  padding: 0 5px;
+  color: #787878;
+  border: 1px solid #ddd;
+  font-size: 12px;
+  border-radius: 3px;
+  margin-right: 10px;
+  margin-bottom: 10px;
+`;
 export const Addtion = styled.div`
-    float:left;
-    display:flex;
-`
+  float: left;
+  display: flex;
+`;
 
 export const Button = styled.div`
-    text-align:center;
-    margin: 9px 5px 0 15px;
+  text-align: center;
+  margin: 9px 5px 0 15px;
+  border-radius: 20px;
+  font-size: 15px;
+  cursor: pointer;
+  &.sign-up {
+    width: 80px;
+    height: 38px;
+    line-height: 38px;
+    border: 1px solid rgba(236, 97, 73, 0.7);
+    color: #ea6f5a;
+    background-color: transparent;
+  }
+  &.write {
+    width: 100px;
+    height: 40px;
+    line-height: 40px;
+    margin: 8px 12px 0;
     border-radius: 20px;
     font-size: 15px;
-    cursor: pointer;
-    &.sign-up{
-    width: 80px;
-        height: 38px;
-        line-height: 38px;
-        border: 1px solid rgba(236,97,73,.7);
-        color: #ea6f5a;
-        background-color: transparent;
-    }
-    &.write{
-        width: 100px;
-        height: 40px;
-        line-height: 40px;
-        margin: 8px 12px 0;
-        border-radius: 20px;
-        font-size: 15px;
-        color: #fff;
-        background-color: #ea6f5a;
-    }
-`
+    color: #fff;
+    background-color: #ea6f5a;
+  }
+`;
